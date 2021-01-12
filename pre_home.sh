03515f8755345a9c7dd887e21e9bf743e93bba0b
Matteo Crippa <matteocrippa@users.noreply.github.com>
2021-01-12 11:18:37 +0100

    debug

--- pre_home.sh
+++ pre_home.sh
@@ -11,19 +11,19 @@ dd if=/dev/urandom of=.ssd_key bs=1024 count=20
 
 echo "Setting up cryptographic volume"
 printf "%s" "$encryption_passphares_home" | cryptsetup -h sha512 -s 512 --use-random --type luks2 luksFormat "$SSD"1
-printf "%s" "$encryption_passphares_home" | cryptsetup luksAddKey "$SSD"1 .ssd_key
+cryptsetup luksAddKey "$SSD"1 .ssd_key
 cryptsetup --key-file .ssd_key luksOpen "$SSD"1 crypthome
 
-echo "Creating physical volume"
-pvcreate /dev/mapper/crypthome
+# echo "Creating physical volume"
+# pvcreate /dev/mapper/crypthome
 
-echo "Creating volume volume"
-vgcreate vg1 /dev/mapper/crypthome
+# echo "Creating volume volume"
+# vgcreate vg1 /dev/mapper/crypthome
 
-echo "Creating logical volumes"
-lvcreate -l +100%FREE vg1 -n home
+# echo "Creating logical volumes"
+# lvcreate -l +100%FREE vg1 -n home
 
-echo "Setting up home partition"
-yes | mkfs.ext4 /dev/vg1/home
-mkdir /mnt/home
-mount /dev/vg1/home /mnt/home
+# echo "Setting up home partition"
+# yes | mkfs.ext4 /dev/vg1/home
+# mkdir /mnt/home
+# mount /dev/vg1/home /mnt/home
