762a8a5bed6b09e6e1d4f517970ad5bc605eab61
Matteo Crippa <matteocrippa@users.noreply.github.com>
2021-01-16 17:43:27 +0100

    fix

--- vars.sh
+++ vars.sh
@@ -6,12 +6,12 @@ export NVME="/dev/nvme0n1"
 export SSD="/dev/sda"
 export PART_ID=$(blkid -o value -s UUID ${SSD}1)
 
-export encryption_passphrase=""
-export root_password=""
+export encryption_passphrase="changeme"
+export root_password="changeme"
 
-export encryption_passphares_home=""
+export encryption_passphares_home="changemetoo"
 export username="matteo"
-export user_password=""
+export user_password="changmetoo"
 
 export hostname="earth"
 export continent_city="Europe/Rome"
